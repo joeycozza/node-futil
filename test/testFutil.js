f76c08357050264586e88b27e65bed23d31b8d30
Andrew Bliss <andrewgbliss@gmail.com>
2014-05-16 15:27:01 -0600

    inital commit

---
+++ test/testFutil.js
@@ -0,0 +1,154 @@
+var expect = require('chai').expect;
+var futil = require('../lib/index.js');
+
+describe('Futil', function(){
+
+	describe('#pathToArray()', function(){
+
+		it('should come back with an array of the default root path', function() {
+
+			var pathArr = futil.pathToArray('/');
+
+			expect(pathArr).to.be.an('array');
+			expect(pathArr).to.have.length.of.at.least(1);
+
+			var rootPath = pathArr[0];
+
+			expect(rootPath).to.be.an('object');
+			expect(rootPath).to.have.a.property('name', 'Root');
+			expect(rootPath).to.have.a.property('path', '/');
+			expect(rootPath).to.have.a.property('isCurrent', true);
+
+		});
+
+		it('should come back with an array of the root path', function() {
+
+			var pathArr = futil.pathToArray('/', 'All Files');
+
+			expect(pathArr).to.be.an('array');
+			expect(pathArr).to.have.length.of.at.least(1);
+
+			var rootPath = pathArr[0];
+
+			expect(rootPath).to.be.an('object');
+			expect(rootPath).to.have.a.property('name', 'All Files');
+			expect(rootPath).to.have.a.property('path', '/');
+			expect(rootPath).to.have.a.property('isCurrent', true);
+
+		});
+
+		it('should come back with an array with two object', function() {
+
+			var pathArr = futil.pathToArray('/test/', 'All Files');
+
+			expect(pathArr).to.be.an('array');
+			expect(pathArr).to.have.length.of.at.least(2);
+
+			var rootPath = pathArr[0];
+
+			expect(rootPath).to.be.an('object');
+			expect(rootPath).to.have.a.property('name', 'All Files');
+			expect(rootPath).to.have.a.property('path', '/');
+			expect(rootPath).to.have.a.property('isCurrent', false);
+
+			var testPath = pathArr[1];
+
+			expect(testPath).to.be.an('object');
+			expect(testPath).to.have.a.property('name', 'test');
+			expect(testPath).to.have.a.property('path', '/test');
+			expect(testPath).to.have.a.property('isCurrent', true);
+
+		});
+
+		it('should come back with an array with three object', function() {
+
+			var pathArr = futil.pathToArray('/test/testing', 'Test');
+
+			expect(pathArr).to.be.an('array');
+			expect(pathArr).to.have.length.of.at.least(3);
+
+			var rootPath = pathArr[0];
+
+			expect(rootPath).to.be.an('object');
+			expect(rootPath).to.have.a.property('name', 'Test');
+			expect(rootPath).to.have.a.property('path', '/');
+			expect(rootPath).to.have.a.property('isCurrent', false);
+
+			var testPath = pathArr[1];
+
+			expect(testPath).to.be.an('object');
+			expect(testPath).to.have.a.property('name', 'test');
+			expect(testPath).to.have.a.property('path', '/test');
+			expect(testPath).to.have.a.property('isCurrent', false);
+
+			var testPath2 = pathArr[2];
+
+			expect(testPath2).to.be.an('object');
+			expect(testPath2).to.have.a.property('name', 'testing');
+			expect(testPath2).to.have.a.property('path', '/test/testing');
+			expect(testPath2).to.have.a.property('isCurrent', true);
+
+		});
+
+	});
+
+	describe('#getTmpFileName()', function(){
+
+		it('should come back with a string for a tmp file name', function(){
+
+			var fileName = futil.getTmpFileName();
+			expect(fileName).to.be.a('string');
+
+		});
+
+		it('should come back with a string for a tmp file name with a extension on the end', function(){
+
+			var fileName = futil.getTmpFileName('tmp');
+			expect(fileName).to.be.a('string');
+			expect(fileName).to.match(/tmp$/);
+
+		});
+
+		it('should come back with a string for a tmp file name with a ./fileTmp in front', function(){
+
+			var fileName = futil.getTmpFileName(null, './fileTmp');
+			expect(fileName).to.be.a('string');
+			expect(fileName).to.match(/^.\/fileTmp/);
+
+		});
+
+	});
+
+	describe('#getExtension()', function(){
+
+		it('should come back tmp as the extension for a full path file name', function(){
+
+			var ext = futil.getExtension('/path/to/fi.le/test.testing.tmp');
+			expect(ext).to.equal('tmp');
+
+		});
+
+		it('should come back tmp as the extension for a file name', function(){
+
+			var ext = futil.getExtension('test.testing.tmp');
+			expect(ext).to.equal('tmp');
+
+		});
+
+		it('should come back nothing as the extension for one name file', function(){
+
+			var ext = futil.getExtension('tmp');
+			expect(ext).to.equal('');
+
+		});
+
+		it('should come back nothing as the extension for hidden file', function(){
+
+			var ext = futil.getExtension('.tmp');
+			expect(ext).to.equal('');
+
+		});
+
+	});
+
+});
